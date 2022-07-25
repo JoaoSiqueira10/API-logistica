71627d65da91a84ae6551947a2380c1a369c96b6
Joao Guilherme <joao.guilherme100@hotmail.com>
2022-07-25 09:25:43 -0300

    Implementacao do CRUD e Exceptions de Cliente

---
+++ logic/src/main/java/com/logistica/logic/domain/repository/ClienteRepository.java
@@ -0,0 +1,18 @@
+package com.logistica.logic.domain.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.logistica.logic.domain.model.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+	List<Cliente> findByNome(String nome);
+	List<Cliente> findByNomeContaining(String nome);
+	Optional<Cliente> findByEmail(String email);
+	
+}
